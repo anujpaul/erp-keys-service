8a4259eda0eb33d6a812fc982af8e7d0d3b9c361
Anuj Paul <anujpaul19@outlook.com>
2026-07-23 21:33:32 -0500

    clean migration

---
+++ src/ERPKeys.Infrastructure/Migrations/20260721193241_SyncCurrentModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERPKeys.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class SyncCurrentModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameIndex(
+                name: "ix_journal_entries_organization_id_source_module_source_document_type_source_document_id",
+                table: "journal_entries",
+                newName: "ix_journal_entries_organization_id_source_module_source_docume");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameIndex(
+                name: "ix_journal_entries_organization_id_source_module_source_docume",
+                table: "journal_entries",
+                newName: "ix_journal_entries_organization_id_source_module_source_document_type_source_document_id");
+        }
+    }
+}
